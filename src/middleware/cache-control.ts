b255c817b269634461154994c5c77f70338c037d
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-16 19:12:18 +0100

    feat: update lint

--- src/middleware/cache-control.ts
+++ src/middleware/cache-control.ts
@@ -1,7 +1,7 @@
-import { ServerResponse } from "http"
-import { CacheHeaders, PageWithCacheHeaders } from "../types"
+import { ServerResponse } from 'http'
+import { CacheHeaders, PageWithCacheHeaders } from '../types'
 
-const cacheControl = (values: CacheHeaders) => (req: Request, res: ServerResponse, next: any) => {
+const cacheControl = (values: CacheHeaders) => (_req: Request, res: ServerResponse, next: any) => {
   const cacheControlValue = Object.entries(values)
     .map(([key, value]) => `${key}=${value}`)
     .join(',')
@@ -12,8 +12,8 @@ const cacheControl = (values: CacheHeaders) => (req: Request, res: ServerRespons
 }
 
 export const createCacheControlMiddleware = (pageWithCacheHeaders: PageWithCacheHeaders) => {
-  const pagePath = pageWithCacheHeaders[0];
-  const cacheHeaders = pageWithCacheHeaders[1];
+  const pagePath = pageWithCacheHeaders[0]
+  const cacheHeaders = pageWithCacheHeaders[1]
 
   return {
     path: pagePath,
--- src/module.ts
+++ src/module.ts
@@ -6,20 +6,19 @@ export default defineNuxtModule<CacheOptions>({
   name: '@nuxt-modules/cache',
   configKey: 'cache',
   setup (options: CacheOptions) {
-
     if (options.browser && !options.browser.pages.length) {
       throw new Error('Missing `browser.pages` option. Provide array of pages you want to apply cache to.')
     }
 
     if (options.browser && options.browser.pages.length) {
-      options.browser.pages.forEach(page => {
+      options.browser.pages.forEach((page) => {
         if (typeof page[0] !== 'string') {
           console.error('First element of page must be a `string`')
-          return;
+          return
         }
         if (typeof page[1] !== 'object') {
           console.error('Second element of page must be an `object`')
-          return;
+          return
         }
 
         addServerMiddleware(createCacheControlMiddleware(page))
