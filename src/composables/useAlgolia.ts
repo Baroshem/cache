41f69a0d8000bb3a5b90afafec077b513b7aa184
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-01-16 19:07:01 +0100

    feat: remove unused composables

--- src/composables/useAlgolia.ts
+++
@@ -1,9 +0,0 @@
-import { SearchClient } from 'algoliasearch/lite'
-import { useNuxtApp } from '#app'
-
-export const useAlgolia = (): SearchClient => {
-  const nuxtApp = useNuxtApp()
-  const algolia: SearchClient = nuxtApp.$algolia
-
-  return algolia
-}
--- src/composables/useInitIndex.ts
+++
@@ -1,8 +0,0 @@
-import { useAlgolia } from './useAlgolia'
-
-export const useInitIndex = (indexName: string) => {
-  const algolia = useAlgolia()
-  const algoliaIndex = algolia.initIndex(indexName)
-
-  return algoliaIndex
-}
